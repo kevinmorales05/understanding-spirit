6953ed32b44b69e3d34205a39fbf8024f39c144e
u548742577 <kdmorale@pichincha.com>
2025-10-14 07:00:46 -0600

    handle the dynamic menu and date

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,16 @@
+//Handle the hamburger menu toggle
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+
+//Get the current year for the copyright
+const currentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.querySelector("#lastModified").textContent = lastModified;
